b17b0d88892499af4623944e4e918e818d4741d7
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-12-12 14:32:37 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitdex::Application.config.secret_key_base = 'aba033003f52dcf65bf64cbed785d4ae33ad188eb3d2f7ae3852fec2f7f8ec8106700f2c5bf1463c1a0a6b5acc9bc08dab59777e9774e07169c24859009612f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twitdex::Application.config.session_store :cookie_store, key: '_twitdex_session'
