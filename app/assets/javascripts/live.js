abd7fb1e439bb3c0b0e02eeda416497363e43704
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-12-18 23:22:58 -0800

    ready for heroku

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -5,28 +5,47 @@ $(function() {
     });
     socket.on('data', function(data) {
 
-      
+      var t = 60
       var time = (data["time"] / 1000)
       if(start == true){
         console.log(data)
-        seriesData = {}  
+        seriesData = { "def": [] }
+        seriesData["def"].push({x: (time-10), y: 0})
+        seriesSetup.push({color: palette.color(), data: seriesData["def"], name: 'def'})
+        for (var i = 1; i <= t; i++) {
+           seriesData["def"].push({x: (seriesData["def"][seriesData["def"].length-1].x + 10), y: 0})
+        };
         for (var key in data["data"]){
           var time = (data["time"] / 1000)
         seriesData[key] = []
         
-        seriesData[key].push({x: time, y: data["data"][key]["sc"]})
+        seriesData[key].push({x: time, y: parseFloat(data["data"][key]["sc"])})
         seriesSetup.push({color: palette.color(), data: seriesData[key], name: key})
         }
-        //console.log(seriesData)
+        
+        console.log(seriesData)
       setup()
       start = false
       }
       
       else {
-        for (var key in data["data"]){
-        seriesData[key].push({x: time, y: data["data"][key]["sc"]})
-
+        if(seriesData["def"].length > 1){
+        seriesData["def"].shift()
       }
+        else {
+          seriesData["def"][0].x = time
+        }
+      
+        for (var key in data["data"]){
+          if(seriesData[key].length >= t){
+            seriesData[key].shift()
+            seriesData[key].push({x: time, y: parseFloat(data["data"][key]["sc"])})
+          }
+          else{
+            seriesData[key].push({x: time, y: parseFloat(data["data"][key]["sc"])})
+          }
+        
+        }
       graph.update()
       }
 
